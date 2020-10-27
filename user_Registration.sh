68dae9438e32b48dee56ab6d7d02ba41519d8ac1
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 13:36:20 +0530

    added code for mobile number validation

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-read -p "enter firstname:" first_name
-firstname_Pattern="^[A-Z][a-z]{2,}$"
-if [[ $first_name =~ $firstname_Pattern ]]
-then
-	echo "valid"
-else
-	echo "invalid"
-fi
-
-read -p "enter lastname:" last_name
-lastname_Pattern="^[A-Z][a-z]{2,}$"
-if [[ $last_name =~ $lastname_Pattern ]]
-then
-        echo "valid"
-else
-        echo "invalid"
-fi
-
-read -p "enter email:" email
-email_Pattern="^[a-zA-Z]{3}.?([a-zA-Z]{3})@[a-zA-Z]{2}.[a-zA-Z]{2}.?([a-zA-Z]{2})?"
-if [[ $email =~ $email_Pattern ]]
+read -p "enter Mobile-Number:" Mobile_number
+MobileNumber_Pattern="^[0-9]{2}[[:space:]][0-9]{10}$"
+if [[ $Mobile_number =~ $MobileNumber_Pattern ]]
 then
         echo "valid"
 else
