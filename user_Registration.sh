a10bc563bde9f69e01275c4a94ec06cf8eee3637
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 13:27:46 +0530

    added code for user to enter a valid email

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+read -p "enter firstname:" first_name
+firstname_Pattern="^[A-Z][a-z]{2,}$"
+if [[ $first_name =~ $firstname_Pattern ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
+
 read -p "enter lastname:" last_name
-lastname_Pattern="^[A-Z][a-z]{3,}$"
+lastname_Pattern="^[A-Z][a-z]{2,}$"
 if [[ $last_name =~ $lastname_Pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
+read -p "enter email:" email
+email_Pattern="^[a-zA-Z]{3}.?([a-zA-Z]{3})@[a-zA-Z]{2}.[a-zA-Z]{2}.?([a-zA-Z]{2})?"
+if [[ $email =~ $email_Pattern ]]
 then
         echo "valid"
 else
