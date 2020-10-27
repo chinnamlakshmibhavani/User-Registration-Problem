60700631d4b7724eaa6628f17669b41acde1ec71
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 13:15:58 +0530

    added code for entering valid first name

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,2 +1,9 @@
-echo "# user_registration"
-
+#!/bin/bash
+read -p "enter firstname:" first_name
+firstname_Pattern="^[A-Z][a-z]{3,}$"
+if [[ $first_name =~ $firstname_Pattern ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
