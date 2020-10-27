209ee1f04324e05c76f071eab7f9a20be8d9e02f
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 14:36:37 +0530

    added code for the password having exactly 1 special character

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "enter password:" Password
-Password_Pattern="^(?=.{10,}$)(?=.*?[A-Z])(?=.*?[0-9]).*$"
+Password_Pattern="^(?=.{10,}$)(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$"
 if [[ $Password =~ $Password_Pattern ]]
 then
 	echo "valid"
